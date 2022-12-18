d91da7f507b3112aa375d25c4bb5ac3cda6b986a
Ivan Mercedes <53806989+ivanmercedes@users.noreply.github.com>
2022-12-18 17:20:56 -0400

    feat: adding context + reducer, ref and some styles fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,20 @@
-import ItemDebug from "./components/ItemDebug";
-import Layout from "./components/Layout";
-
-const { ipcRenderer } = window.require("electron");
+import Empty from './components/Empty';
+import ItemDebug from './components/ItemDebug';
+import Layout from './components/Layout';
+import { useAppContext } from './context/app-context/app-provider';
 
 function App() {
-  const testEvent = () => {
-    ipcRenderer.send("test-event", JSON.stringify({ name: "pepe" }));
-  };
+	const { logs } = useAppContext();
 
-  const showLogs = false;
+	if (logs.length === 0) return <Empty />;
 
-  if (showLogs) {
-    return (
-      <Layout>
-        <ItemDebug color="border-logy-green" />
-        <ItemDebug color="border-logy-yellow" />
-        <ItemDebug color="border-logy-red" />
-        <ItemDebug color="border-logy-blue" log="hola" />
-      </Layout>
-    );
-  }
-
-  return (
-    <Layout>
-      <div className="h-full grid place-content-center text-white ">
-        <h1 className="text-5xl font-bold text-white/50 text-center">No Logs available</h1>
-        <div className="flex flex-col gap-2 mt-20">
-          <div className="flex gap-2 items-center">
-            <img src="tips.svg" alt="tips" className="w-4" />
-            <span className="text-2xl font-semibold">Tips</span>
-          </div>
-          <label>
-            Press
-            <span className="bg-white/20 rounded-lg mx-2 font-semibold"> Shift + Window + L </span>
-            to show and hide Logy
-          </label>
-        </div>
-      </div>
-    </Layout>
-  );
+	return (
+		<Layout>
+			{logs.map(log => (
+				<ItemDebug key={log.id} log={log} />
+			))}
+		</Layout>
+	);
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  theme: {
-    extend: {
-      colors: {
-        'logy-browm': '#12131a',
-        'logy-green': '#72d364',
-        'logy-red': '#d36364',
-        'logy-yellow': '#d3bb64',
-        'logy-orange': '#FBDD73',
-        'logy-blue': '#629dd4',
-        'logy-card': '#23242A',
-      },
-    },
-  },
-  plugins: [],
-}
+	content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+	theme: {
+		extend: {
+			colors: {
+				'logy-browm': '#12131a',
+				'logy-green': '#72d364',
+				'logy-red': '#d36364',
+				'logy-yellow': '#d3bb64',
+				'logy-orange': '#FBDD73',
+				'logy-blue': '#629dd4',
+				'logy-card': '#23242A',
+			},
+		},
+	},
+	plugins: [],
+};
