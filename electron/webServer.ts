326b99e5872ff4f51fc69c85630ef5595e661c42
Ivan Mercedes <53806989+ivanmercedes@users.noreply.github.com>
2022-12-19 10:32:45 -0400

    style: import in body of module; reorder to top

--- electron/webServer.ts
+++ electron/webServer.ts
@@ -1,20 +1,20 @@
-const express = require("express");
-import { BrowserWindow } from "electron";
-import { Response, Request } from "express";
+import { BrowserWindow } from 'electron';
+import { Response, Request } from 'express';
+const express = require('express');
 
 const server = express();
 
 export const createServer = (window: BrowserWindow) => {
-  server.use(express.json());
-  server.post("/log", (req: Request, res: Response) => {
-    console.log(req.body);
+	server.use(express.json());
+	server.post('/log', (req: Request, res: Response) => {
+		console.log(req.body);
 
-    window.webContents.send("log", JSON.stringify(req.body));
+		window.webContents.send('log', JSON.stringify(req.body));
 
-    res.status(204).send();
-  });
+		res.status(204).send();
+	});
 
-  server.listen(9723, () => {
-    console.log(`Webserver running at port ${9723}`);
-  });
+	server.listen(9723, () => {
+		console.log(`Webserver running at port ${9723}`);
+	});
 };
